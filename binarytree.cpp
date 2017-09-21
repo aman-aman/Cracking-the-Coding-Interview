ef0870b7733e9c1f09d503bb4664aed2b7f5d702
Aman Kumar Jha <31754652+amanjha1@users.noreply.github.com>
2017-09-21 23:23:27 +0530

    Add files via upload

---
+++ binarytree.cpp
@@ -0,0 +1,27 @@
+/*
+aman kumar jha
+*/
+vector<int> n;
+void inOrderTraversal(Node* node)
+ {
+	if(node == NULL)
+	{
+			return;
+	}
+	inOrderTraversal(node->left);
+	nodes.push_back(node->data);
+	inOrderTraversal(node->right);
+}
+
+bool checkBST(Node* root)
+{
+	inOrderTraversal(root);
+	for(int i = 1; i < nodes.size(); i++)
+	{
+		if(nodes[i] <= nodes[i-1])
+		{
+			return false;
+		}
+	}
+	return true;
+}
